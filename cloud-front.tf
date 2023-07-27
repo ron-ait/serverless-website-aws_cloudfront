dc293f510289d1ecdc8ad4ce9477d4b4a491a6a7
ron-ait <100356208+ron-ait@users.noreply.github.com>
2023-07-27 20:10:33 +0530

    Update cloud-front.tf

--- cloud-front.tf
+++ cloud-front.tf
@@ -1,23 +1,28 @@
+# Configure the AWS provider with the desired region
 provider "aws" {
   region = "us-east-1"
 }
 
+# Define a variable for the domain name with a default value
 variable "domain_name_simple" {
-  type = string 
-  default = "new-domain.tk"  
+  type    = string
+  default = "new-domain.tk"
 }
 
+# Create an S3 bucket to store the static website files
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "rohit-bucket-devops-tf"
 }
 
+# Configure AWS S3 bucket access permissions
 resource "aws_s3_account_public_access_block" "website_bucket" {
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+  block_public_acls         = true
+  block_public_policy       = true
+  ignore_public_acls        = true
+  restrict_public_buckets   = true
 }
 
+# Upload the index.html file to the S3 bucket
 resource "aws_s3_object" "website_bucket" {
   bucket       = aws_s3_bucket.website_bucket.id
   key          = "index.html"
@@ -25,19 +30,24 @@ resource "aws_s3_object" "website_bucket" {
   content_type = "text/html"
 }
 
+# Create an AWS CloudFront distribution for the static website
 resource "aws_cloudfront_distribution" "cdn_static_site" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
   comment             = "my cloudfront in front of the s3 bucket"
 
+  # Configure the origin for the CloudFront distribution
   origin {
     domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_id                = "my-s3-origin"
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
   }
-  
-   aliases = ["new-domain.tk"]
+
+  # Define aliases (CNAMEs) for the CloudFront distribution
+  aliases = ["new-domain.tk"]
+
+  # Configure default cache behavior for the distribution
   default_cache_behavior {
     min_ttl                = 0
     default_ttl            = 0
@@ -56,6 +66,7 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
     }
   }
 
+  # Configure restrictions for the CloudFront distribution
   restrictions {
     geo_restriction {
       locations        = []
@@ -63,63 +74,71 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
     }
   }
 
+  # Configure SSL certificate for the distribution
   viewer_certificate {
-      acm_certificate_arn      = aws_acm_certificate.cert.arn
-      ssl_support_method       = "sni-only"
-      minimum_protocol_version = "TLSv1.2_2021"
-    }
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 }
+
+# Create an SSL certificate using AWS ACM (Amazon Certificate Manager)
 resource "aws_acm_certificate" "cert" {
-    domain_name               = var.domain_name_simple
-    validation_method         = "DNS"
-    subject_alternative_names = [var.domain_name_simple]
+  domain_name               = var.domain_name_simple
+  validation_method         = "DNS"
+  subject_alternative_names = [var.domain_name_simple]
 
-    lifecycle {
-      create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Fetch the Route53 hosted zone information for the specified domain name
 data "aws_route53_zone" "zone" {
-    provider = aws
-    name         = var.domain_name_simple
-    private_zone = false
+  provider     = aws
+  name         = var.domain_name_simple
+  private_zone = false
 }
+
+# Create Route53 records to validate the SSL certificate
 resource "aws_route53_record" "cert_validation" {
-    provider = aws
-    for_each = {
-      for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-        name   = dvo.resource_record_name
-        record = dvo.resource_record_value
-        type   = dvo.resource_record_type
-      }
+  provider = aws
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
+  }
 
-    allow_overwrite = true
-    name            = each.value.name
-    records         = [each.value.record]
-    type            = each.value.type
-    zone_id         = data.aws_route53_zone.zone.zone_id
-    ttl             = 60
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
+  ttl             = 60
 }
 
+# Validate the SSL certificate
 resource "aws_acm_certificate_validation" "cert" {
-    certificate_arn         = aws_acm_certificate.cert.arn
-    validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
-
+# Create an A record for the apex domain that points to the AWS CloudFront distribution
 resource "aws_route53_record" "apex" {
-    zone_id = data.aws_route53_zone.zone.id
-    name    = var.domain_name_simple
-    type    = "A"
+  zone_id = data.aws_route53_zone.zone.id
+  name    = var.domain_name_simple
+  type    = "A"
 
-    alias {
-      name                   = aws_cloudfront_distribution.cdn_static_site.domain_name
-      zone_id                = aws_cloudfront_distribution.cdn_static_site.hosted_zone_id
-      evaluate_target_health = false
-    }
+  alias {
+    name                   = aws_cloudfront_distribution.cdn_static_site.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn_static_site.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
+# Create an AWS CloudFront origin access control for signing requests
 resource "aws_cloudfront_origin_access_control" "default" {
   name                              = "cloudfront OAC"
   description                       = "description of OAC"
@@ -128,10 +147,12 @@ resource "aws_cloudfront_origin_access_control" "default" {
   signing_protocol                  = "sigv4"
 }
 
+# Output the CloudFront distribution URL
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.cdn_static_site.domain_name
 }
 
+# Generate IAM policy document to allow CloudFront to access the S3 bucket
 data "aws_iam_policy_document" "website_bucket" {
   statement {
     actions   = ["s3:GetObject"]
@@ -148,6 +169,7 @@ data "aws_iam_policy_document" "website_bucket" {
   }
 }
 
+# Attach the IAM policy to the S3 bucket
 resource "aws_s3_bucket_policy" "website_bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.id
   policy = data.aws_iam_policy_document.website_bucket.json
